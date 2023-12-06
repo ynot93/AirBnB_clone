960c92decc2e5dddef88b833106075b8b498eb99
Wiztech-01 <lynnnyaoke@gmail.com>
2023-12-06 05:26:41 +0000

    basemodel dictionaries

--- models/base_model.py
+++ models/base_model.py
@@ -18,9 +18,15 @@ class BaseModel():
         Initializes objects with the following instance attributes.
 
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key = '__class__':
+                    continue
+                elif key in ['created_at', 'updated_at']:
+                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                else:
+                    self.id = str(uuid.uuid4())
+                    self.created_at = datetime.now()
 
     def __str__(self):
         """
