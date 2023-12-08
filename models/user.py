874c14dad4b932cf2e0afa3b870d61033be5f583
Wiztech-01 <lynnnyaoke@gmail.com>
2023-12-08 05:15:01 +0000

    user info

--- models/user.py
+++ models/user.py
@@ -1,6 +1,14 @@
 #!usr/bin/python3
 
+from models import storage
 from models.base_model import BaseModel
+from models.user import User
+
+#all_objs = storage.all()
+#print("-- Reloaded objects --")
+#for obj_id in all_objs.keys():
+    #obj = all_objs[obj_id]
+    # print(obj)
 
 Class user(BaseModel):
 
