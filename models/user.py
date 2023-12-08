36074485bd476be9afd9c3aaa57ee613dcc3c836
Wiztech-01 <lynnnyaoke@gmail.com>
2023-12-08 05:11:21 +0000

    first commit

---
+++ models/user.py
@@ -0,0 +1,14 @@
+#!usr/bin/python3
+
+from models.base_model import BaseModel
+
+Class user(BaseModel):
+
+    """
+    Class that handles the users information
+    """
+
+    email = ""
+    pasword = ""
+    first_name = ""
+    last_name = ""
