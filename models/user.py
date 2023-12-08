d9bd9ba166784e11c8038b42317513340a53a8ff
Wiztech-01 <lynnnyaoke@gmail.com>
2023-12-08 05:58:03 +0000

    Create user class

--- models/user.py
+++ models/user.py
@@ -2,15 +2,8 @@
 
 from models import storage
 from models.base_model import BaseModel
-from models.user import User
 
-#all_objs = storage.all()
-#print("-- Reloaded objects --")
-#for obj_id in all_objs.keys():
-    #obj = all_objs[obj_id]
-    # print(obj)
-
-Class user(BaseModel):
+Class User(BaseModel):
 
     """
     Class that handles the users information
@@ -20,3 +13,9 @@ Class user(BaseModel):
     pasword = ""
     first_name = ""
     last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initializes  a class object
+        """
+        super().__init__(*args, **kwargs)
